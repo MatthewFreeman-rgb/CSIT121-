37035f23ee7c28fb34bf9f35576cdceed7047539
MatthewFreeman-rgb <fr1224855@email.ccbcmd.edu>
2026-04-20 20:43:34 -0400

    Delete Lab08 directory

--- Lab08/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    var nav = document.getElementById("myNav");
-
-    if (nav.className === "topnav") {
-        nav.className += " responsive";
-    } else {
-        nav.className = "topnav";
-    }
-}
